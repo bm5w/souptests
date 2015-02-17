efc89a6858782da6e27f1f5abf32b4acc390e2ac
Mark Saiget <saiget@gmail.com>
2015-02-16 17:33:53 -0800

    Add get_inspection_page method to scrape entire listing.

---
+++ scraper.py
@@ -0,0 +1,36 @@
+from bs4 import BeautifulSoup
+import requests
+soup = BeautifulSoup()
+
+
+INSPECTION_DOMAIN = 'http://info.kingcounty.gov'
+INSPECTION_PATH = '/health/ehs/foodsafety/inspections/Results.aspx'
+INSPECTION_PARAMS = {
+    'Output': 'W',
+    'Business_Name': '',
+    'Business_Address': '',
+    'Longitude': '',
+    'Latitude': '',
+    'City': '',
+    'Zip_Code': '',
+    'Inspection_Type': 'All',
+    'Inspection_Start': '',
+    'Inspection_End': '',
+    'Inspection_Closed_Business': 'A',
+    'Violation_Points': '',
+    'Violation_Red_Points': '',
+    'Violation_Descr': '',
+    'Fuzzy_Search': 'N',
+    'Sort': 'H'
+}
+
+
+def get_inspection_page(**kwargs):
+    url = INSPECTION_DOMAIN + INSPECTION_PATH
+    params = INSPECTION_PARAMS.copy()
+    for key, value in kwargs.items():
+        if key in INSPECTION_PARAMS:
+            params[key] = value
+    resp = requests.get(url, params=params)
+    resp.raise_for_status()
+    return resp.content, resp.encoding
