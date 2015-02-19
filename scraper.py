0c4259c0d94050075268a8497f22d49d7283bba7
Mark Saiget <saiget@gmail.com>
2015-02-18 22:19:47 -0800

    Add options input parameters so user defines sort method, number of restaurants, and whether to reverse the list.

--- scraper.py
+++ scraper.py
@@ -2,6 +2,9 @@ from bs4 import BeautifulSoup
 import requests
 import sys
 import re
+import geocoder
+import json
+import argparse
 
 
 INSPECTION_DOMAIN = 'http://info.kingcounty.gov'
@@ -128,23 +131,73 @@ def extract_score_data(elem):
     return data
 
 
-if __name__ == '__main__':
+def generate_results(test=False, count=10):
     kwargs = {
         'Inspection_Start': '2/1/2013',
         'Inspection_End': '2/1/2015',
         'Zip_Code': '98109'
     }
-    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+    if test:
         html, encoding = load_inspection_page('load_inspection.html')
     else:
         html, encoding = get_inspection_page(**kwargs)
     doc = parse_source(html, encoding)
     listings = extract_data_listing(doc)
 
-    with open('final_output.txt', 'w') as outfile:
-        for listing in listings[:5]:
-            metadata = extract_restaurant_metadata(listing)
-            score_data = extract_score_data(listing)
-            metadata.update(score_data)
-            outfile.write("{}\n".format(metadata))
-        outfile.close()
+    for listing in listings[:count]:
+        metadata = extract_restaurant_metadata(listing)
+        score_data = extract_score_data(listing)
+        metadata.update(score_data)
+        yield metadata
+
+
+def get_geojson(result):
+    address = " ".join(result.get('Address', ''))
+    if not address:
+        return None
+    geocoded = geocoder.google(address)
+    geojson = geocoded.geojson
+    inspection_data = {}
+    use_keys = (
+        'Business Name', 'Average Score', 'Total Inspections', 'High Score',
+        'Address',
+    )
+    for key, val in result.items():
+        if key not in use_keys:
+            continue
+        if isinstance(val, list):
+            val = " ".join(val)
+        inspection_data[key] = val
+    new_address = geojson['properties'].get('address')
+    if new_address:
+        inspection_data['Address'] = new_address
+    geojson['properties'] = inspection_data
+    return geojson
+
+
+def sort_(total_result, sort='average', reverse=False):
+    sort_options = {'average': 'Average Score', 'highscore': 'High Score', 'most': 'Total Inspections'}
+    return sorted(total_result, reverse=reverse, key=lambda k: k['properties'][sort_options.get(sort)])
+
+
+if __name__ == '__main__':
+    import pprint
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('sort')
+    parser.add_argument('num', type=int)
+    parser.add_argument('reverse')
+    args = parser.parse_args()
+    if args.reverse == 'reverse':
+        reverse = True
+    else:
+        reverse = False
+    total_result = {'type': 'FeatureCollection', 'features': []}
+    for result in generate_results(count=args.num):
+        geo_result = get_geojson(result)
+        total_result['features'].append(geo_result)
+    pprint.pprint('xxx: {}'.format(total_result['features']))
+    total_result['features'] = sort_(total_result['features'], args.sort, reverse)
+    pprint.pprint('xxx: {}'.format(total_result['features']))
+    with open('my_map.json', 'w') as fh:
+        json.dump(total_result, fh)
